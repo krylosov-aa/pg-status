138246529e25512a056ce8934c2ca38692a6275f
krylosov-aa <krylosov.andrew@gmail.com>
2026-01-19 23:48:34 +0300

    remove unnecessary thread utils wrappers

--- src/pg_monitor/pg_monitor.c
+++ src/pg_monitor/pg_monitor.c
@@ -72,25 +72,25 @@ static void *pg_monitor_thread(void *arg) {
 
     check_hosts();
 
-    mutex_lock(&start_mutex);
+    pthread_mutex_lock(&start_mutex);
     pg_monitor_ready = true;
-    cond_broadcast(&start_cond);
-    mutex_unlock(&start_mutex);
+    pthread_cond_broadcast(&start_cond);
+    pthread_mutex_unlock(&start_mutex);
 
     struct timespec ts;
-    mutex_lock(&stop_mutex);
+    pthread_mutex_lock(&stop_mutex);
     while (is_running()) {
         clock_gettime(CLOCK_REALTIME, &ts);
         ts.tv_sec += parameters.sleep;
 
-        cond_timedwait(&stop_cond, &stop_mutex, &ts);
+        pthread_cond_timedwait(&stop_cond, &stop_mutex, &ts);
         if (!is_running()) {
             break;
         };
 
         check_hosts();
     }
-    mutex_unlock(&stop_mutex);
+    pthread_mutex_unlock(&stop_mutex);
     return nullptr;
 }
 
@@ -98,20 +98,20 @@ static void *pg_monitor_thread(void *arg) {
  * Starts a host monitoring thread
  */
 pthread_t start_pg_monitor() {
-    mutex_lock(&start_mutex);
+    pthread_mutex_lock(&start_mutex);
     const int started = pthread_create(
         &monitor_tid, nullptr, pg_monitor_thread, nullptr
     );
 
     if (started != 0) {
-        mutex_unlock(&start_mutex);
+        pthread_mutex_unlock(&start_mutex);
         raise_error("Failed to start pg_monitor");
     }
 
     while (!pg_monitor_ready) {
-        cond_wait(&start_cond, &start_mutex);
+        pthread_cond_wait(&start_cond, &start_mutex);
     }
-    mutex_unlock(&start_mutex);
+    pthread_mutex_unlock(&start_mutex);
     printf("pg_monitor started\n");
     return monitor_tid;
 }
@@ -129,10 +129,10 @@ void stop_pg_monitor(void) {
         &monitor_running, false, memory_order_relaxed
     );
 
-    mutex_lock(&stop_mutex);
-    cond_broadcast(&stop_cond);
-    mutex_unlock(&stop_mutex);
+    pthread_mutex_lock(&stop_mutex);
+    pthread_cond_broadcast(&stop_cond);
+    pthread_mutex_unlock(&stop_mutex);
 
-    thread_join(monitor_tid, nullptr);
+    pthread_join(monitor_tid, nullptr);
     printf("pg_monitor stopped\n");
 }
